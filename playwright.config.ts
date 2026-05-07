f197355a4c8907e996a9d8ab898dbfe0be91665d
rickcedwhat-ai <ccata002+ai@gmail.com>
2026-05-07 18:26:02 -0400

    feat: setup library infrastructure

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  // Only look for E2E tests in the /tests directory
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
